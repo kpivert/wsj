0afe15a329a1e43aad8f29339ddfe6aec32467bc
Kurtis Pivert <kurtis.pivert@gmail.com>
2025-07-28 13:52:24 -0400

    First Pass at Economist Choropleth

---
+++ 05_choropleth/01.R
@@ -0,0 +1,363 @@
+# 2025-07-27 Economist Choropleth in ggplot2
+
+# 00 Libraries ------------------------------------------------------------
+
+require(tabulapdf)
+require(systemfonts)
+require(extrafont)
+loadfonts(quiet = TRUE)
+require(patchwork)
+require(tidyverse)
+
+# 01 Data -----------------------------------------------------------------
+
+hilltop_df <-
+  extract_tables(
+    file = "05_choropleth/Eliminating Provider Taxes May 6 2025.pdf",
+    pages = 2
+  )
+
+locate_areas(
+  file = "05_choropleth/Eliminating Provider Taxes May 6 2025.pdf",
+  pages = 2
+)
+
+first_table <- c(122.40887, 52.40394, 584.73399, 297.22167)
+
+second_table <- c(120.4581, 304.0493, 569.1281, 563.4975)
+
+tbl_part_1 <-
+  extract_tables(
+    file = "05_choropleth/Eliminating Provider Taxes May 6 2025.pdf",
+    pages = 2, 
+    area = list(first_table), 
+    guess = FALSE
+  )
+
+tbl_part_2 <-
+  extract_tables(
+    file = "05_choropleth/Eliminating Provider Taxes May 6 2025.pdf",
+    pages = 2, 
+    area = list(second_table), 
+    guess = FALSE
+  ) 
+
+tbl_part_1 <-
+  tbl_part_1 |> 
+  pluck(1) |> 
+  set_names(paste0("col_", 1:3))
+
+tbl_part_2 <-
+  tbl_part_2 |> 
+  pluck(1) |> 
+  select(1, 3, 5) |> 
+  set_names(paste0("col_", 1:3))  
+
+# 02 Cleaned Data ---------------------------------------------------------
+
+tbl_part_1_cleaned_df <-
+  tbl_part_1 |> 
+  slice(4:29) |> 
+  mutate(
+    col_3 = str_extract(
+      col_3,
+      "([0-9\\.\\%])*$"
+    )
+  ) |> 
+  select(
+    State = col_1,
+    Pct = col_3
+  ) |> 
+  mutate(
+    Pct = parse_number(Pct)
+  )
+
+tbl_part_2_cleaned_df <-
+  tbl_part_2 |> 
+    mutate(
+      State = str_extract(
+        col_1,
+        "([A-Z]){2}"
+      ),
+      Pct = parse_number(col_3)
+    ) |> 
+    select(
+      State, Pct
+    )
+
+df <-
+  bind_rows(
+    tbl_part_1_cleaned_df,
+    tbl_part_2_cleaned_df
+  )
+
+# write_rds(
+#   df, 
+#   file = "05_choropleth/hilltop.rds"
+# )
+
+df <-
+  read_rds(
+    file = "05_choropleth/hilltop.rds"
+  )
+
+# 03 Viz ------------------------------------------------------------------
+
+# Economist is 8 x 11 Square Grid
+
+viz_df <-
+  tribble(
+    ~State, ~x, ~y,
+    "AL", 7, 7, 
+    "AK", 1, 1,
+    "AZ", 2, 6,
+    "AR", 5, 6,
+    "CA", 1, 5,
+    "CO", 3, 5,
+    "CT", 10, 4,
+    "DC", 9, 6,
+    "DE", 10, 5,
+    "FL", 9, 8,
+    "GA", 8, 7,
+    "HI", 1, 8,
+    "ID", 2, 3,
+    "IL", 6, 3,
+    "IN", 6, 4,
+    "IA", 5, 4,
+    "KS", 4, 6,
+    "KY", 6, 5,
+    "LA", 5, 7,
+    "ME", 11, 1,
+    "MD", 9, 5,
+    "MA", 10, 3,
+    "MI", 7, 3,
+    "MN", 5, 3,
+    "MS", 6, 7,
+    "MO", 5, 5,
+    "MT", 3, 3,
+    "NE", 4, 5,
+    "NV", 2, 4,
+    "NH", 11, 2, 
+    "NJ", 9, 4, 
+    "NM", 3, 6, 
+    "NY", 9, 3, 
+    "NC", 7, 6, 
+    "ND", 4, 3, 
+    "OH", 7, 4,
+    "OK", 4, 7,
+    "OR", 1, 4,
+    "PA", 8, 4,
+    "RI", 11, 4,
+    "SC", 8, 6,
+    "SD", 4, 4,
+    "TN", 6, 6,
+    "TX", 4, 8,
+    "UT", 2, 5,
+    "VT", 10, 2,
+    "VA", 8, 5,
+    "WA", 1, 3,
+    "WV", 7, 5,
+    "WI", 6, 2,
+    "WY", 3, 4
+  ) |> 
+  mutate(
+    y = case_when(
+      y == 1 ~ 12, 
+      y == 2 ~ 11, 
+      y == 3 ~ 10, 
+      y == 4 ~ 9, 
+      y == 5 ~ 8, 
+      y == 6 ~ 7, 
+      y == 7 ~ 6, 
+      y == 8 ~ 5, 
+      y == 9 ~ 4, 
+      y == 10 ~ 3, 
+      y == 11 ~ 2, 
+      y == 12 ~ 1
+    )
+  ) 
+
+pal <-
+  c("#EF1B26", "#F58469", "#FAAA90", "#FCC8B4", "#EEECE6", "#EDEBE3")
+
+ggplot(
+  viz_df,
+  aes(
+    x = x, 
+    y = y, 
+    group = State
+    )
+  ) +
+  geom_tile(
+    # fill = "#fff",
+    fill = pal[6],
+    color = "#000"
+  ) +
+  theme_void() +
+  coord_equal() +
+  geom_text(
+    aes(
+      label = State
+    )
+  ) +
+  theme(
+    plot.background = element_rect(
+      color = pal[6],
+      fill = pal[6]
+    ),
+    panel.background = element_rect(
+      fill = pal[6],
+      color = pal[6]
+    )
+  ) 
+
+a <-
+  viz_df |> 
+    left_join(
+      df, 
+      by = "State"
+    ) |> 
+    mutate(
+      fill_color = case_when(
+        between(Pct, 30, 40) ~ pal[1],
+        between(Pct, 20, 30) ~ pal[2],
+        between(Pct, 10, 20) ~ pal[3],
+        between(Pct, 0.1, 10) ~ pal[4],
+        Pct == 0 ~ pal[5]
+      )
+    ) |>
+    mutate(
+      text_color = if_else(
+        fill_color == pal[1], 
+        "#ffffff",
+        "#000000"
+      )
+    ) |> 
+    ggplot(
+      aes(
+        x = x, 
+        y = y
+      )
+    ) +
+    geom_tile(
+      aes(
+        fill = fill_color
+      ),
+      color = "#000"
+    ) +
+    scale_fill_identity() +
+    theme_void() +
+    coord_equal() +
+    geom_text(
+      aes(
+        label = State,
+        color = text_color
+      ),
+      family = "Arial Narrow", 
+      size = 3.4
+    ) +
+    scale_color_identity() +
+    labs(
+      caption = "Source: The Hilltop Institute"
+    ) +
+    theme(
+      plot.caption = element_text(hjust = 0.1),
+      plot.background = element_rect(
+        color = pal[6],
+        fill = pal[6]
+      ),
+      panel.background = element_rect(
+        fill = pal[6],
+        color = pal[6]
+      ),
+      text = element_text(family = "Arial Narrow")
+    ) 
+
+
+a +
+  labs(
+    title = "   Federal Medicaid funding from hospital & nursing-\nhome taxes, forecast for fiscal year 2026, %"
+  ) +
+  theme(
+    plot.title = element_text(
+      size = 10,
+      face = "bold",
+      hjust = 0.2
+    ),
+    plot.title.position = "plot"
+  )
+
+# ggsave(
+#   filename = "05_choropleth/first_take_2.png",
+#   width = 3,
+#   height = 3,
+#   units = "in",
+#   dpi = 300
+# )
+
+scale_df <-
+  tibble(
+    x = seq(10, 40, 10),
+    y = rep(0.1, 4),
+    fill_color = c(pal[4], pal[3], pal[2], pal[1])
+  )
+
+top_legend <-
+  ggplot(
+    scale_df,
+    aes(
+      x = x, 
+      y = y
+    )
+  ) +
+  geom_tile(
+    color = "#000000",
+    aes(
+      fill = fill_color
+    ),
+    linewidth = 0.4
+  ) +
+  scale_fill_identity() +
+  theme_void(
+    base_family = "Arial Narrow"
+  ) +
+  expand_limits(
+    y = 10
+  ) +
+  scale_x_continuous(
+    breaks = seq(15, 45, 10),
+    labels = seq(10, 40, 10)  |> as.character()
+  ) +
+  theme(
+    axis.text.x.bottom = element_text(
+      vjust = 0.5,
+      size = 4
+    ),
+    plot.background = element_rect(
+      color = pal[6],
+      fill = pal[6]
+    ),
+    panel.background = element_rect(
+      fill = pal[6],
+      color = pal[6]
+    )
+  )
+
+# a +
+#   labs(
+#     title = "   Federal Medicaid funding from hospital & nursing-\nhome taxes, forecast for fiscal year 2026, %"
+#   ) +
+#   guides(
+#     fill = "legend"
+#   ) +
+#   theme(
+#     plot.title = element_text(
+#       size = 10,
+#       face = "bold",
+#       hjust = 0.2
+#     ),
+#     plot.title.position = "plot",
+#     legend.position = "top"
+#   )
+
+top_legend / a
---
+++ 05_choropleth/hilltop.md
@@ -0,0 +1,374 @@
+May 6, 2025
+
+What’s the Impact of Eliminating Medicaid Provider Taxes?
+
+Medicaid is a joint state-federal program, and states are required to fund a portion of total
+spending. States are currently permitted to tax health care providers up to 6% to help fund the
+state share. A potential Medicaid reform would lower this threshold—potentially to 0%—and
+thus reduce federal Medicaid funding for many states. The Congressional Budget Oﬃce estimates
+that reducing the threshold to 0% would lead to $612 billion in federal savings over 10 years.
+
+How would this impact federal Medicaid spending in individual states?
+
+How do provider taxes work?
+
+Almost every state has at least one type of provider
+tax. States use provider tax revenue to partially
+ﬁnance their share of total Medicaid spending.
+
+Provider tax revenue and federal matching revenue,
+based on the Federal Medical Assistance Percentage
+(FMAP), is then returned to the state’s health system
+and used for a variety of purposes.
+
+Currently, states are permitted to eﬀectively collect up to 6% of net patient revenue per provider
+class. This is referred to as the “safe harbor” limit.
+
+States with high FMAP & high provider tax rates would be
+most aﬀected if the safe harbor limit is removed.
+
+The impact of removing the 6% provider tax threshold
+depends on each state’s provider tax rates and FMAP:
+  Higher provider tax rates translate to a higher
+
+state share of Medicaid spending.
+
+  Higher FMAP translates to a higher federal
+
+match on the state share and thus more total
+Medicaid funding.
+
+Data and methods
+Provider tax rates are imputed from Kaiser Family Foundation. We use the lower bound of each tax rate range
+for states with a provider tax in place and assume a 1.0% rate if it is presented as ≤3.5%. Although 19 classes
+of providers can be subject to provider taxes, we focus on nursing facilities (NFs) and hospitals. Therefore,
+these estimates likely understate true eﬀects.
+
+Total net patient revenue data by state for general, short-term hospitals are from FY 2023 Medicare Cost
+Reports and FY 2021 Medicare Cost Reports for nursing facilities. We calculate blended FY 2023 FMAPs and
+use FY 2023 total Medicaid expenditure from the Medicaid Budget and Expenditure System, and use
+traditional FMAPs from Kaiser Family Foundation. Where necessary, we use a 3% growth rate assumption to
+trend quantities to FY 2026.
+
+If provider taxes are eliminated, at least $83.7 billion in annual
+federal Medicaid funding is at risk in FY 2026.
+
+Lost Federal Medicaid Spending ($ Millions)*
+
+Due to
+Hospital
+Taxes
+
+$1,868.2
+$2,714.0
+$1,296.9
+$2,047.7
+$502.8
+$2,700.8
+$1,399.6
+$1,036.2
+$1,269.9
+$3,356.2
+$4,856.9
+$4,885.8
+$1,978.1
+$4,758.8
+$319.7
+$2,527.9
+$2,955.4
+$1,326.7
+$1,741.4
+$2,837.2
+$3,673.8
+$6,739.9
+$379.9
+$1,398.5
+$271.0
+
+Due to
+NF
+Taxes
+
+$241.7
+$0.0
+$38.9
+$236.8
+$25.3
+$269.1
+$197.4
+$25.4
+$62.6
+$317.9
+$359.0
+$31.0
+$167.8
+$607.4
+$18.6
+$89.4
+$32.8
+$138.5
+$73.0
+$0.0
+$361.6
+$67.9
+$49.8
+$97.0
+$58.2
+
+Hospital
++ NF
+
+$2,109.9
+$2,714.0
+$1,335.8
+$2,284.5
+$528.1
+$2,969.9
+$1,597.0
+$1,061.6
+$1,332.5
+$3,674.1
+$5,215.9
+$4,916.8
+$2,145.9
+$5,366.2
+$338.3
+$2,617.3
+$2,988.2
+$1,465.2
+$1,814.4
+$2,837.2
+$4,035.4
+$6,807.8
+$429.7
+$1,495.5
+$329.2
+
+State
+
+% of
+Total
+Federal
+Share
+37.90%  WV
+37.86%
+NC
+35.90%
+NY
+35.57%  WY
+32.05%
+OH
+30.33%  ME
+30.19%
+KY
+29.63%
+HI
+28.53%
+GA
+27.56%
+AR
+27.43%  WI
+25.88%
+NJ
+25.58%
+DC
+23.12%
+PA
+23.04%
+LA
+21.61%  MD
+21.41%  MA
+20.72%
+CA
+20.13%  WA
+18.39%
+DE
+17.99%
+NM
+17.46%
+NE
+17.00%
+SD
+11.87%
+AK
+10.71%
+ND
+
+Due to
+Hospital
+Taxes
+
+$299.4
+$968.1
+$5,631.9
+$29.8
+$1,296.5
+$165.6
+$704.8
+$112.7
+$701.4
+$296.7
+$426.8
+$546.4
+$131.1
+$889.1
+$609.9
+$270.8
+$417.3
+$2,385.5
+$491.8
+$0.0
+$0.0
+$0.0
+$0.0
+$0.0
+$0.0
+
+Due to NF
+Taxes
+
+Hospital
++ NF
+
+% of Total
+Federal
+Share
+
+$166.9
+$473.5
+$498.1
+$9.0
+$752.5
+$76.1
+$235.1
+$43.7
+$57.7
+$179.4
+$30.1
+$278.7
+$44.0
+$571.5
+$47.6
+$169.3
+$163.4
+$716.1
+$81.8
+$44.3
+$86.0
+$13.5
+$0.0
+$0.0
+$0.0
+
+$466.3
+$1,441.6
+$6,130.0
+$38.8
+$2,049.0
+$241.7
+$939.9
+$156.4
+$759.1
+$476.1
+$456.9
+$825.1
+$175.1
+$1,460.6
+$657.5
+$440.1
+$580.7
+$3,101.6
+$573.6
+$44.3
+$86.0
+$13.5
+$0.0
+$0.0
+$0.0
+
+9.53%
+9.49%
+9.32%
+8.15%
+8.11%
+7.44%
+6.59%
+6.52%
+6.30%
+6.20%
+5.48%
+5.27%
+5.01%
+4.98%
+4.71%
+3.82%
+3.81%
+3.50%
+2.63%
+1.71%
+1.17%
+0.48%
+0.00%
+0.00%
+0.00%
+
+$186.2
+
+$22.7
+
+$208.9
+
+10.06%
+
+Total  $75,405.1
+
+$8,328.1
+
+$83,733.2
+
+12.92%
+
+State
+
+MS
+SC
+UT
+AL
+NH
+TN
+IA
+KS
+NV
+MO
+MI
+AZ
+OK
+FL
+VT
+OR
+IN
+CT
+CO
+VA
+IL
+TX
+RI
+MN
+ID
+
+MT
+
+What does this mean?
+Based on tax revenue collected from hospitals and nursing facilities, states will collect at least $83.7
+billion in federal funding for their Medicaid programs in FY 2026. If the safe harbor threshold is
+eliminated, states will be required to ﬁnd alternative sources of state Medicaid funding, or else lose
+this federal funding. This will not impact states equally. The elimination of the 6% safe harbor
+threshold means that several states (Mississippi, South Carolina, Utah, Alabama, New Hampshire,
+Tennessee, and Iowa) could lose over 30% of their total Medicaid federal funding.
+
+Notes: Florida did not report tax rates, but we imputed nursing facility and hospital tax rates based on correspondence. While this analysis accounts for tax revenue from hospital and
+nursing facilities, there are other potential sources of tax revenue that we do not include in this analysis. Additionally, the CBO assumes that states will replace half of the lost provider tax
+revenues with other sources of state funding, but this analysis does not include this assumption. Authors: Morgan Henderson (mhenderson@hilltop.umbc.edu), Leigh Goetschius, and
+Alice Middleton. With sincere thanks to Dunc Williams of the Medical University of South Carolina for providing the state-level nursing facility net patient revenue data.
+
+
